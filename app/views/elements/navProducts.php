96c3680bb474361d1221e5e81c295db166e6b6b6
TennNiedziedz <krawczyk_wojciech@wp.pl>
2022-11-29 10:58:34 +0100

    Extraction of Products navbar from layouts to elements/navProducts

---
+++ app/views/elements/navProducts.php
@@ -0,0 +1,38 @@
+<div class="h4 row">
+  <div class="col">Produkty</div>
+  <div class="col text-end">
+    <a class="link-warning" href="#" data-bs-toggle="tooltip" data-bs-placement="left" data-bs-title="Ulubione"><i class="bi bi-heart-fill"></i></a>
+    <a class="link-warning" href="/addpost" data-bs-toggle="tooltip" data-bs-placement="left" data-bs-title="Dodaj wpis"><i class="bi bi-plus-circle-fill"></i></a>
+  </div>
+</div>
+<a href="/postlist?product=1" class="nav-link">Synergia</a>
+<a href="/postlist?product=2" class="nav-link">e-Sekretariat</a>
+<a href="/postlist?product=3" class="nav-link">e-Świadectwa</a>
+<a class="nav-link bi bi-chevron-right collapse-toggle d-inline-flex align-items-center collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#mobile-collapse" aria-expanded="false">
+    Aplikacje mobilne
+</a>
+<div class="collapse" id="mobile-collapse">
+    <a href="#" class="nav-link">Librus - uczniowie i rodzice</a>
+    <a href="#" class="nav-link">Nasze szkoły</a>
+    <a href="#" class="nav-link">Librus - nauczyciel</a>
+</div>
+
+<a class="nav-link bi bi-chevron-right collapse-toggle d-inline-flex align-items-center collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#asc-collapse" aria-expanded="false">
+    Produkty aSc
+</a>
+<div class="collapse" id="asc-collapse">
+    <a href="#" class="nav-link">aSc Plan lekcji</a>
+    <a href="#" class="nav-link">e Plan lekcji</a>
+    <a href="#" class="nav-link">EduPage</a>
+</div>
+
+<a class="nav-link bi bi-chevron-right collapse-toggle d-inline-flex align-items-center collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#arch-collapse" aria-expanded="false">
+    Produkty archiwalne
+</a>
+<div class="collapse" id="arch-collapse">
+    <a href="#" class="nav-link">Świadectwa dekstopowe</a>
+    <a href="#" class="nav-link">Sekretariat desktopowy</a>
+    <a href="#" class="nav-link">Biblioteka desktopowa</a>
+    <a href="#" class="nav-link">Stołówka</a>
+    <a href="#" class="nav-link">e-Nauczanie</a>
+</div>
--- app/views/layouts/admin.php
+++ app/views/layouts/admin.php
@@ -52,44 +52,7 @@
           <div id="sideNavbar" style="width:100%; overflow: hidden;" data-simplebar >
             <div class="navbar-nav mb-2 p-3">
 
-              <div class="h4 row">
-                <div class="col">Produkty</div>
-                <div class="col text-end">
-                  <a class="link-warning" href="#" data-bs-toggle="tooltip" data-bs-placement="left" data-bs-title="Ulubione"><i class="bi bi-heart-fill"></i></a>
-                  <a class="link-warning" href="/addpost" data-bs-toggle="tooltip" data-bs-placement="left" data-bs-title="Dodaj wpis"><i class="bi bi-plus-circle-fill"></i></a>
-                </div>
-              </div>
-              <a class="nav-link" href="#">Synergia</a>
-              <a class="nav-link" href="#">e-Sekretariat</a>
-              <a class="nav-link" href="#">e-Świadectwa</a>
-              <a class="nav-link bi bi-chevron-right collapse-toggle d-inline-flex align-items-center collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#mobile-collapse" aria-expanded="false">
-                  Aplikacje mobilne
-              </a>
-              <div class="collapse" id="mobile-collapse">
-                  <a href="#" class="nav-link">Librus - uczniowie i rodzice</a>
-                  <a href="#" class="nav-link">Nasze szkoły</a>
-                  <a href="#" class="nav-link">Librus - nauczyciel</a>
-              </div>
-
-              <a class="nav-link bi bi-chevron-right collapse-toggle d-inline-flex align-items-center collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#asc-collapse" aria-expanded="false">
-                  Produkty aSc
-              </a>
-              <div class="collapse" id="asc-collapse">
-                  <a href="#" class="nav-link">aSc Plan lekcji</a>
-                  <a href="#" class="nav-link">e Plan lekcji</a>
-                  <a href="#" class="nav-link">EduPage</a>
-              </div>
-
-              <a class="nav-link bi bi-chevron-right collapse-toggle d-inline-flex align-items-center collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#arch-collapse" aria-expanded="false">
-                  Produkty archiwalne
-              </a>
-              <div class="collapse" id="arch-collapse">
-                  <a href="#" class="nav-link">Świadectwa dekstopowe</a>
-                  <a href="#" class="nav-link">Sekretariat desktopowy</a>
-                  <a href="#" class="nav-link">Biblioteka desktopowa</a>
-                  <a href="#" class="nav-link">Stołówka</a>
-                  <a href="#" class="nav-link">e-Nauczanie</a>
-              </div>
+              <?php include_once  Application::$ROOT_DIR."/views/elements/navProducts.php"; ?>
 
               <hr>
               <div class="h4">Pozostałe</div>
