4936a396c6aaa6b2db7784bd04431115db4cc1ef
TennNiedziedz <krawczyk_wojciech@wp.pl>
2022-11-30 15:21:34 +0100

    Added comments for Response.php

--- app/core/Response.php
+++ app/core/Response.php
@@ -4,11 +4,21 @@ namespace app\core;
 
 class Response
 {
+  /**
+   * Sets http response code.
+   * 
+   * @param int $code
+   */
   public function setStatusCode(int $code)
   {
     http_response_code($code);
   }
 
+  /**
+   * Redirects to given url.
+   * 
+   * @param string $url
+   */
   public function redirect(string $url)
   {
     header('Location: '.$url);
