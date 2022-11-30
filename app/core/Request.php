fd4db2ccdb99ec39911bef7b1c5d2bc48926b5a1
TennNiedziedz <krawczyk_wojciech@wp.pl>
2022-11-30 14:42:11 +0100

    Added comments in Request.php

--- app/core/Request.php
+++ app/core/Request.php
@@ -4,6 +4,12 @@ namespace app\core;
 
 class Request
 {
+
+  /**
+   * Returns path from request URI.
+   * 
+   * @return string Part of URL after domain, before '?'
+   */
   public function getPath()
   {
     $path = $_SERVER['REQUEST_URI'] ?? '/';
@@ -14,11 +20,21 @@ class Request
     return substr($path, 0, $position);
   }
 
+  /**
+   * Returns method form request.
+   * 
+   * @return string
+   */
   public function getMethod()
   {
     return strtolower($_SERVER['REQUEST_METHOD']);
   }
 
+  /**
+   * Pulls out params from URI.
+   * 
+   * @return string Part of URL after '?'
+   */
   public function getParams()
   {
     $path = $_SERVER['REQUEST_URI'] ?? '/';
@@ -29,27 +45,42 @@ class Request
     return substr($path, ($position+1), strlen($path));
   }
 
+  /**
+   * Checks if request method is GET.
+   * 
+   * @return bool True if method is GET
+   */
   public function isGet()
   {
     return $this->getMethod() === 'get';
   }
 
+  /**
+   * Checks if request method is POST.
+   * 
+   * @return bool True if method is POST
+   */
   public function isPost()
   {
     return $this->getMethod() === 'post';
   }
 
+  /**
+   * Sanatize params send in request and store them in array.
+   * 
+   * @return array $body
+   */
   public function getBody()
   {
     $body = [];
 
-    if($this->getMethod() === 'get') {
+    if($this->isGet()) {
       foreach ($_GET as $key => $value) {
         $body[$key] = filter_input(INPUT_GET, $key, FILTER_SANITIZE_SPECIAL_CHARS);
       }
     }
 
-    if($this->getMethod() === 'post') {
+    if($this->isPost()) {
       foreach ($_POST as $key => $value) {
         $body[$key] = filter_input(INPUT_POST, $key, FILTER_SANITIZE_SPECIAL_CHARS);
       }
