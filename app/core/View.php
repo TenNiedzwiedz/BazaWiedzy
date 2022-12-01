0bd894e8456d044314fada48a6df094beaa46295
TennNiedziedz <krawczyk_wojciech@wp.pl>
2022-12-01 11:09:25 +0100

    Clean up

--- app/core/View.php
+++ app/core/View.php
@@ -6,12 +6,12 @@ class View
 {
   public function renderView($view, $params = [])
   {
-    $layoutContent = $this->layoutContent($params);
-    $viewContent = $this->viewContent($view, $params);
+    $layoutContent = $this->renderLayout($params);
+    $viewContent = $this->renderContent($view, $params);
     return str_replace('{{content}}', $viewContent, $layoutContent);
   }
 
-  protected function layoutContent($params)
+  protected function renderLayout($params)
   {
     $layout = Application::$app->layout;
     foreach ($params as $key => $value) {
@@ -22,7 +22,7 @@ class View
     return ob_get_clean();
   }
 
-  protected function viewContent($view, $params)
+  protected function renderContent($view, $params)
   {
     foreach ($params as $key => $value) {
       $$key = $value;
