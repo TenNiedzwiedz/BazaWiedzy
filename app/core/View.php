3d71c1575db0f011d90c51b95303df3dda2128bb
TennNiedziedz <krawczyk_wojciech@wp.pl>
2022-12-01 11:18:03 +0100

    Added comments for View.php

--- app/core/View.php
+++ app/core/View.php
@@ -4,14 +4,29 @@ namespace app\core;
 
 class View
 {
-  public function renderView($view, $params = [])
+  /**
+   * Generates View for given parameters.
+   * 
+   * @param string $view
+   * @param array $params
+   * 
+   * @return string Generated view
+   */
+  public function renderView(string $view, array $params = []) : string
   {
     $layoutContent = $this->renderLayout($params);
     $viewContent = $this->renderContent($view, $params);
     return str_replace('{{content}}', $viewContent, $layoutContent);
   }
 
-  protected function renderLayout($params)
+  /**
+   * Generates layout set in Application->layout.
+   * 
+   * @param array $params
+   * 
+   * @return string View layout
+   */
+  protected function renderLayout(array $params) : string
   {
     $layout = Application::$app->layout;
     foreach ($params as $key => $value) {
@@ -22,7 +37,15 @@ class View
     return ob_get_clean();
   }
 
-  protected function renderContent($view, $params)
+  /**
+   * Generates content for given view.
+   * 
+   * @param string $view
+   * @param array $params
+   * 
+   * @return string View content
+   */
+  protected function renderContent(string $view, array $params) : string
   {
     foreach ($params as $key => $value) {
       $$key = $value;
