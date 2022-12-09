39f4c9d3b6fd1ca8fa9a020e5efc6ddce4cdbf03
TennNiedziedz <krawczyk_wojciech@wp.pl>
2022-12-09 08:42:17 +0100

    Redone flash messages

--- app/views/elements/flashMessages.php
+++ app/views/elements/flashMessages.php
@@ -4,29 +4,46 @@
 
 <div id="flashMessages">
   <div class="toast-container position-fixed bottom-0 end-0 p-3">
-    <?php if($flashMessage = Application::$app->session->checkFlash()): ?>
-      <div id="FlashMessage" class="toast text-white bg-<?= $flashMessage['type'] ?> bg-opacity-50 border-0" data-bs-delay="7500" role="alert" aria-live="assertive" aria-atomic="true">
-        <div class="toast-header text-white bg-<?= $flashMessage['type'] ?> bg-opacity-50">
+      <div id="flashMessage" class="toast text-white bg-opacity-50 border-0" data-bs-delay="7500" role="alert">
+        <div id ="flashHeader" class="toast-header text-white bg-opacity-50">
           <i class="bi bi-bell-fill"></i>
-          <strong class="me-auto">&nbsp;<?= $flashMessage['label'] ?></strong>
-          <button type="button" class="btn-close btn-close-white" data-bs-dismiss="toast" aria-label="Close"></button>
+          <strong id="flashTitle" class="me-auto">&nbsp;</strong>
+          <button type="button" class="btn-close btn-close-white" data-bs-dismiss="toast"></button>
         </div>
-        <div class="toast-body">
-          <?= $flashMessage['message'] ?>
+        <div id="flashBody" class="toast-body">
         </div>
       </div>
-    <?php endif; ?>
   </div>
 </div>
 
 <script>
-  function showFlashMessage()
+  var flashMessage = <?= json_encode(Application::$app->session->checkFlash()) ?>;
+
+  function showFlashMessage(message = false)
   {
-    const flashMessageToast = document.getElementById('FlashMessage')
-    if (flashMessageToast) {
+    if(message) {
+      flashMessage = JSON.parse(message);
+      console.log(flashMessage);
+    }
+    const flashMessageToast = document.getElementById('flashMessage');
+    const flashHeader = document.getElementById('flashHeader');
+    const flashTitle = document.getElementById('flashTitle');
+    const flashBody = document.getElementById('flashBody');
+
+    if (flashMessage) {
       const toast = new bootstrap.Toast(flashMessageToast);
 
-      toast.show()
+      const types = ['success', 'warning', 'danger'];
+      types.forEach(function (type) {
+        flashMessageToast.classList.remove('bg-'+type);
+        flashHeader.classList.remove('bg-'+type);
+      });
+      flashMessageToast.classList.add('bg-'+flashMessage.type);
+      flashHeader.classList.add('bg-'+flashMessage.type);
+      flashTitle.innerHTML = '&nbsp;'+flashMessage.label;
+      flashBody.innerText = flashMessage.message;
+
+      toast.show();
     }
   }
   </script>
